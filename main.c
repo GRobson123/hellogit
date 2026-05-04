f336bd6e8a9e235250f5f2ec69a20a855a3ca593
Grobson123 <george2.robson@live.uwe.ac.uk>
2026-05-04 22:00:23 +0100

    Initial commit: basic C project

--- main.c
+++ main.c
@@ -8,19 +8,50 @@
 
 
 float data[8][MAX_columns];
-void record();
 void recordfr();
-void printfunc();
-void addfunc(int z);
-void rms(int y);
-void catchfunc(int x);
+int dcfunc(int z);
+float rms(int y);
+int catchfunc(int x);
+void rangefunc(int p);
+float ptpfunc(int t);
 int count=0;
+float resultf;
+int resulti;
+float minimum;
+float maximum;
+
 int main(void) {
 
     recordfr();
-    rms(1);
-    rms(2);
-    rms(3);
+    FILE *fz = fopen("results.txt","w");
+    if (fz == NULL) {
+        printf("error opening file \n");
+        return 1;
+    }
+    fprintf(fz,"results\n");
+    resultf = rms(1);
+    fprintf(fz,"rms of phase a is: %.3f\n", resultf);
+    resultf = ptpfunc(1);
+    fprintf(fz,"peak to peak of phase a is: %.1f\n", resultf);
+    resulti = dcfunc(1);
+    fprintf(fz,"dc offset of phase a is: %d\n", resulti);
+    resultf = rms(2);
+    fprintf(fz,"rms of phase b is: %.3f\n", resultf);
+    resultf = rms(3);
+    fprintf(fz,"rms of phase c is: %.3f\n", resultf);
+    resulti = catchfunc(1);
+    fprintf(fz,"number of clipped readings of phase a is: %d\n", resulti);
+    resulti = catchfunc(2);
+    fprintf(fz,"number of clipped readings of phase b is: %d\n", resulti);
+    resulti = catchfunc(3);
+    fprintf(fz,"number of clipped readings of phase c is: %d\n", resulti);
+    rangefunc(5);
+    fprintf(fz,"the frequency range is: %.3f - %.3f \n",minimum,maximum);
+    rangefunc(6);
+    fprintf(fz,"the power factor range is: %.3f - %.3f \n",minimum,maximum);
+    rangefunc(7);
+    fprintf(fz,"the thd range is: %.3f - %.3f \n",minimum,maximum);
+    fclose(fz);
 
 }
 
@@ -28,7 +59,7 @@ void recordfr(){
     FILE *fp = fopen("power_quality_log.csv","r");
     if (fp == NULL) {
         printf("error opening file \n");
-        return ;
+        return;
     }
 
     char line[256];
@@ -63,72 +94,21 @@ void recordfr(){
         data[7][count] = atof(token);
 
         count++;
-
-
-
     }
     fclose(fp);
 }
-void record(){
-    FILE *fp = fopen("test.csv","r");
-    if (fp == NULL) {
-        printf("error opening file \n");
-        return ;
-    }
 
-    char line[256];
-
-    fgets(line, sizeof(line), fp);
-
-    while (fgets(line, sizeof(line), fp) !=NULL){
-        char *token;
-
-        token = strtok(line,",");
-        data[0][count] = atof(token);
-
-        token = strtok(NULL,",");
-        data[1][count] = atof(token);
-
-        token = strtok(NULL,",");
-        data[2][count] = atof(token);
-
-        token = strtok(NULL,",");
-        data[3][count] = atof(token);
-
-        token = strtok(NULL,",");
-        data[4][count] = atof(token);
-
-        token = strtok(NULL,",");
-        data[5][count] = atof(token);
-
-        token = strtok(NULL,",");
-        data[6][count] = atof(token);
-
-        token = strtok(NULL,",");
-        data[7][count] = atof(token);
-
-        count++;
-
-
-
-    }
-    fclose(fp);
-}
-void printfunc(){
-    for (int i = 0; i < 8; i++) {
-        printf("Collumn %d: %.1f %.1f %.1f\n",
-               i, data[i][0], data[i][1], data[i][2]);
-    }
-    return;
-}
-void addfunc(z){
+int dcfunc(z){
     int sum=0;
+    int dc;
     for (int i = 0; i<1000; i++) {
         sum += data[z][i];
     }
-    printf("total = %d \n", sum);
+    dc = sum/1000;
+    printf("dc offset = %d \n", dc);
+    return dc;
 }
-void rms(y){
+float rms(y){
     float sqrsum=0;
     int cnt = 0;
     float fnl;
@@ -138,8 +118,9 @@ void rms(y){
     }
     fnl = sqrt(sqrsum/cnt);
     printf("rms = %.3f \n", fnl);
+    return fnl;
 }
-void catchfunc(x) {
+int catchfunc(x) {
     int catch = 0;
     for (int i = 0; i<1000; i++) {
         if(data[x][i]>=325){
@@ -150,4 +131,42 @@ void catchfunc(x) {
         }
     }
     printf("the catch num is: %d \n",catch);
+    return catch;
 }
+void rangefunc(p){
+    float high =0;
+    float low=100;
+    for (int i = 0; i<1000; i++) {
+        if (data[p][i]>high){
+            high=data[p][i];
+        }
+        if (data[p][i]<low){
+            low=data[p][i];
+        }
+    }
+    printf("the range max is: %.3f \n",high);
+    printf("the range min: %.3f \n",low);
+    maximum = high;
+    minimum = low;
+
+
+}
+
+float ptpfunc(t){
+    float high =0;
+    float low=1000;
+    float ptp;
+    for (int i = 0; i<1000; i++) {
+        if (data[t][i]>high){
+            high=data[t][i];
+        }
+        if (data[t][i]<low){
+            low=data[t][i];
+        }
+    }
+    ptp= high- low;
+    printf("the peak to peak is: %.1f \n",ptp);
+    return ptp;
+
+}
+
